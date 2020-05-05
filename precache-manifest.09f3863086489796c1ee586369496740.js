bea27ccff96899c762f16042f2405b41270e3b4a
sapyati <sapyati.smtm@gmail.com>
2020-05-05 17:43:17 +0200

    Add files via upload

---
+++ precache-manifest.09f3863086489796c1ee586369496740.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac6f968cdae10abc57b4f9a78a36211",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc6129942f833d0b673e",
+    "url": "/static/css/main.17fecb34.chunk.css"
+  },
+  {
+    "revision": "a3935ad4855fe9767806",
+    "url": "/static/js/2.88ca853f.chunk.js"
+  },
+  {
+    "revision": "7f7b7554ad8c1d8206ae278b825bcae0",
+    "url": "/static/js/2.88ca853f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc6129942f833d0b673e",
+    "url": "/static/js/main.517352a5.chunk.js"
+  },
+  {
+    "revision": "f185516d00916ec4d74a",
+    "url": "/static/js/runtime-main.4f258c25.js"
+  },
+  {
+    "revision": "1549b40d8886e5cc29c0f00cf0573fc1",
+    "url": "/static/media/blob.1549b40d.svg"
+  }
+]);
